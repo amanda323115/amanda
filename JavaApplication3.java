d4e071d93b862fbbc8d72155b8393f1d9713108e
amanda323115 <amandavinagre.ba@gmail.com>
2024-10-01 11:20:41 -0300

    Update JavaApplication1.java

--- JavaApplication3.java
+++ JavaApplication3.java
@@ -32,3 +32,25 @@ class Carro {
         System.out.println("O carro" + marca + " " + nome + " esta andando");
     }
     }
+
+class Carro {
+  private String marca;
+  private String nome;
+  public Carro(String marca, String nome) {
+    this.marca = marca;
+    this.nome = nome;
+  }
+  protected String nomeCompleto() {
+    return marca + " " + nome;
+  }
+  public void andar() {
+    System.out.println("O carro " + nomeCompleto() + " está andando");
+  }
+}
+public class App {
+  public static void main(String[] args) {
+    Carro carro = new Carro("Fiat", "Gol");
+    carro.marca = "VW"; // Irá apresentar um erro de compilação
+    carro.andar();
+  }
+}
